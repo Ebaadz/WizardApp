c632f46991673b59b33fccbf0c1622ce68892e3a
Ebaad Zaidi <zaidieb@sheridancollege.ca>
2024-08-15 13:59:03 -0400

    third commit

--- src/app/wizard/wizard.component.ts
+++ src/app/wizard/wizard.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { wizarddata } from '../wizarddata.service';
 
 @Component({
   selector: 'app-wizard',
   standalone: true,
   templateUrl: './wizard.component.html',
-  styleUrls: ['./wizard.component.css']
+  styleUrls: ['./wizard.component.css'],
+  imports: [CommonModule]
 })
 export class WizardComponent implements OnInit {
   wizardData: any;
@@ -15,7 +17,6 @@ export class WizardComponent implements OnInit {
   ngOnInit(): void {
     this.wizarddata.getWizardData().subscribe(data => {
       this.wizardData = data;
-
     });
   }
 }
